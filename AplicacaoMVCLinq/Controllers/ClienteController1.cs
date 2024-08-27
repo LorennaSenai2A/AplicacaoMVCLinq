1470ec75781edc9b08e2fa8fc63e738d85a85479
LorennaSenai2A <lorennasenaia@gmail.com>
2024-08-27 16:10:59 -0300

    Criando as informações e listagem dos clientes

---
+++ AplicacaoMVCLinq/Controllers/ClienteController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacaoMVCLinq.Controllers
+{
+    public class ClienteController1 : Controller
+    {
+        public IActionResult GetCliente()
+        {
+            Cliente c1 = new Cliente(1, "Lorena", "lorena.victor@aluno.senai.br", 121212, new DateOnly(2008, 02, 12));
+            Cliente c2 = new Cliente(2, "Juliana", "juliana.fulanetto@aluno.senai.br", 909090, new DateOnly(2008, 01, 15));
+            Cliente c3 = new Cliente(3, "Maysa", "maysa.rondon@aluno.senai.br", 454545, new DateOnly(2008, 06, 07));
+            Cliente c4 = new Cliente(4, "Md", "matheus.dias@aluno.senai.br", 707070, new DateOnly(2009, 03, 08));
+            Cliente c5 = new Cliente(5, "Mavi", "maria.vitoria@aluno.senai.br", 262626, new DateOnly(2011, 08, 26));
+
+            //Criar uma lista de clientes
+            List<Cliente> listaClientes = new List<Cliente>();
+
+            listaClientes.Add(a1);
+            listaClientes.Add(a2);
+            listaClientes.Add(a3);
+            listaClientes.Add(a4);
+            listaClientes.Add(a5);
+
+            return View(listaClientes);
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ AplicacaoMVCLinq/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace AplicacaoMVCLinq.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int RM { get; set; }
+
+
+        public Cliente(int id, string nome, string email, int rm)
+        {
+            this.ClienteId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.RM = rm;
+        }
+    }
+}
