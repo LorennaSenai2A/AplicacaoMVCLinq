2774e296fa23d57df33d18a6371f09ba04375c0e
LorennaSenai2A <lorennasenaia@gmail.com>
2024-08-28 08:04:11 -0300

    Arrumando os nomes dos clientes

--- AplicacaoMVCLinq/Controllers/ClienteController1.cs
+++ AplicacaoMVCLinq/Controllers/ClienteController1.cs
@@ -12,9 +12,9 @@ namespace AplicacaoMVCLinq.Controllers
 
         public IActionResult GetCliente()
         {
-            Cliente c1 = new Cliente(1, "Lorena", "lorena.victor@aluno.senai.br", 121212, new DateOnly(2008, 02, 12));
-            Cliente c2 = new Cliente(2, "Juliana", "juliana.fulanetto@aluno.senai.br", 909090, new DateOnly(2008, 01, 15));
-            Cliente c3 = new Cliente(3, "Maysa", "maysa.rondon@aluno.senai.br", 454545, new DateOnly(2008, 06, 07));
+            Cliente c1 = new Cliente(1, "Loh", "lorena.victor@aluno.senai.br", 121212, new DateOnly(2008, 02, 12));
+            Cliente c2 = new Cliente(2, "Juju", "juliana.fulanetto@aluno.senai.br", 909090, new DateOnly(2008, 01, 15));
+            Cliente c3 = new Cliente(3, "May", "maysa.rondon@aluno.senai.br", 454545, new DateOnly(2008, 06, 07));
             Cliente c4 = new Cliente(4, "Md", "matheus.dias@aluno.senai.br", 707070, new DateOnly(2009, 03, 08));
             Cliente c5 = new Cliente(5, "Mavi", "maria.vitoria@aluno.senai.br", 262626, new DateOnly(2011, 08, 26));
 
